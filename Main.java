e259832f4dc749873d37abf96aaad7e38a4d504f
nagarajuobbineni96 <32006988+nagarajuobbineni96@users.noreply.github.com>
2017-09-16 12:48:34 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int n;
+		int i;
+		Scanner snr=new Scanner(System.in);
+		n=snr.nextInt();
+		int students[] = null;
+		for(i=0;i<n;i++)
+		{
+			students[i]=snr.nextInt();
+		}
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
